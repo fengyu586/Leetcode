a0ff848c48855b787c20319366de2bf606cc4d66
fengyu586 <1349156262@qq.com>
2019-04-08 18:03:04 +0800

    # update

---
+++ Array/16. 3Sum Closest.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3.6
+# -*- coding:utf-8 -*-
+# https://leetcode.com/problems/3sum-closest/
+# tips: 1.two pointer
+
+class Solution:
+    def threeSumClosest(self, nums, target):
+        n = len(nums)
+        nums.sort()
+        res = sum(nums[:3])
+        for i in range(n-1):
+            l, r = i+1, n-1
+            while l < r:
+                s = nums[i]+nums[l]+nums[r]
+                if abs(s-target) < abs(res-target):
+                    res = s
+                elif s < target:
+                    l += 1
+                elif s > target:
+                    r -= 1
+                else:
+                    return res
+        return res
+
+
+
+
