ca7e482c445cb7b72e376ac8dda910e993e2e151
fengyu586 <1349156262@qq.com>
2019-04-08 15:37:25 +0800

    # add two pointer Approach and brute force Approach

--- Array/11 Container With Most Water.py
+++ Array/11 Container With Most Water.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3.6
 # -*- coding:utf-8 -*-
 # https://leetcode.com/problems/container-with-most-water/
+# maxArea1 is two point approach
+# maxArea2 暴力法 result: Time Limit Exceeded
+
 
 class Solution:
-    def maxArea(self, height):
+    def maxArea1(self, height):
         n = len(height)
-        max_val = 0
+        max_area = 0
         start, end = 0, n-1
         while end != start:
-            max_val = max(min(height[start], height[end])*(end-start), max_val)
+            max_area = max(min(height[start], height[end])*(end-start), max_area)
             if height[end] > height[start]:
                 start += 1
             else:
                 end -= 1
-        return max_val
+        return max_area
 
-
-
-
-
+    def maxArea2(self, height):
+        max_area = 0
+        n = len(height)
+        for i in range(n):
+            for j in range(i+1, n):
+                max_area = max(min(height[i], height[j])*(j-i), max_area)
+        return max_area
 
 
