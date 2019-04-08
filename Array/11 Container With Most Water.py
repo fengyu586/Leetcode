fbe5025893b53f19ec7e2d29f8ee8f4cf222943f
fengyu586 <1349156262@qq.com>
2019-04-08 14:11:46 +0800

    # add two pointer Approach

--- Array/11 Container With Most Water.py
+++ Array/11 Container With Most Water.py
@@ -1,2 +1,23 @@
 #!/usr/bin/python3.6
-# -*- coding:utf-8 -*-  
+# -*- coding:utf-8 -*-
+# https://leetcode.com/problems/container-with-most-water/
+
+class Solution:
+    def maxArea(self, height):
+        n = len(height)
+        max_val = 0
+        start, end = 0, n-1
+        while end != start:
+            max_val = max(min(height[start], height[end])*(end-start), max_val)
+            if height[end] > height[start]:
+                start += 1
+            else:
+                end -= 1
+        return max_val
+
+
+
+
+
+
+
