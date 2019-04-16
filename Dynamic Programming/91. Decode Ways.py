399541aaf5bf1c7c32d3bf43cda7dedaad9cf2fc
fengyu586 <1349156262@qq.com>
2019-04-16 21:39:54 +0800

    # medium

--- Dynamic Programming/91. Decode Ways.py
+++ Dynamic Programming/91. Decode Ways.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3.6
 # -*- coding:utf-8 -*-
 # https://leetcode.com/problems/decode-ways/
-# 分治法：两个两个考虑
+# Approach1: 分治法：两个两个考虑, 时间复杂度：O(n)，空间复杂度：O(1)
+# Approach2: 时间复杂度：O(n)， 空间复杂度：O(n)
 
 
 class Solution:
-    def numDecodings(self, s):
+    def numDecodings1(self, s):
         if not s or s == '0':
             return 0
         n = len(s)
@@ -28,8 +29,20 @@ class Solution:
                     merge, done = 0, merge+done
         return merge+done
 
+    def numDecodings2(self, s):
+        if not s:
+            return 0
+        n = len(s)
+        dp = [1] + [0] * n
+        for i in range(1, n + 1):
+            if s[i - 1] != '0':
+                dp[i] += dp[i - 1]
+            if i > 1 and 10 <= int(s[i - 2: i]) <= 26:
+                dp[i] += dp[i - 2]
+        return dp[n]
+
 
 if __name__ == '__main__':
     s = Solution()
     a = '226'
-    print(s.numDecodings(a))
+    print(s.numDecodings2(a))
