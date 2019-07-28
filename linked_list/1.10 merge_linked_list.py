9a3a5f6fe8ace3cf4655b5839c976f08d26e33bc
fengyu586 <1349156262@qq.com>
2019-07-28 22:19:25 +0800

    # medium

--- linked_list/1.10 merge_linked_list.py
+++ linked_list/1.10 merge_linked_list.py
@@ -18,9 +18,9 @@ class LNode:
         self.next = None
 
 
-def construct_list():
+def construct_list(start):
     """构造链表"""
-    i = 1
+    i = start
     head = LNode()
     tmp = None
     cur = head
@@ -40,12 +40,51 @@ def print_list(head):
         cur = cur.next
 
 
+def merge(head1, head2):
+    if head1 is None or head1.next is None:
+        return head2
+    if head2 is None or head2.next is None:
+        return head1
+    cur1 = head1.next               # 用来遍历head1
+    cur2 = head2.next               # 用来遍历head2
+    head = None                     # 合并后链表的头结点
+    cur = None                      # 合并后的链表在尾结点
+    if cur1.data > cur2.data:
+        head = head2
+        cur = cur2
+        cur2 = cur2.next
+    else:
+        head = head1
+        cur = cur1
+        cur1 = cur1.next
+
+    # 每次找链表剩余结点的最小值对应的结点连接到合并后链表的尾部
+    while cur1 is not None and cur2 is not None:
+        if cur1.data < cur2.data:
+            cur.next = cur1
+            cur = cur1
+            cur1 = cur1.next
+        else:
+            cur.next = cur2
+            cur = cur2
+            cur2 = cur2.next
+    # 当遍历完一个链表后把另外一个链表剩余的结点链接到合并后的链表后面。
+    if cur1 is not None:
+        cur.next = cur1
+    if cur2 is not None:
+        cur.next = cur2
+    return head
 
 
-
-
-
-
-
+if __name__ == '__main__':
+    head1 = construct_list(1)
+    head2 = construct_list(2)
+    print("head1: ")
+    print_list(head1)
+    print("head2: ")
+    print_list(head2)
+    print("合并后的链表：")
+    head = merge(head1, head2)
+    print_list(head)
 
 
