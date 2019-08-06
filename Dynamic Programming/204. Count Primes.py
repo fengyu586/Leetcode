e379316de1bfaf5756b301b1a2295f153d3e5068
fengyu586 <1349156262@qq.com>
2019-08-07 01:18:46 +0800

    # medium

---
+++ Dynamic Programming/204. Count Primes.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""====================================
+# @Time    : 2019/8/7 1:17
+# @Author  : Jing
+# @FileName: 204. Count Primes.py
+# @IDE: PyCharm
+======================================="""
+# https://leetcode.com/problems/count-primes/
+
+
+class Solution:
+    def countPrimes(self, n):
+        if n <= 2:
+            return 0
+        res = [True]*n
+        res[0] = res[1] = False
+        for i in range(2, n):
+            if res[i] is True:
+                for j in range(2, (n-1)//i+1):
+                    res[i*j] = False
+        return sum(res)
+
+
