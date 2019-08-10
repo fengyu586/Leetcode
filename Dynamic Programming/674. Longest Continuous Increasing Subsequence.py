b189741de5054692d3552a914d3c56a04826c6a6
fengyu586 <1349156262@qq.com>
2019-08-10 16:56:17 +0800

    # easy

--- Dynamic Programming/674. Longest Continuous Increasing Subsequence.py
+++ Dynamic Programming/674. Longest Continuous Increasing Subsequence.py
@@ -6,3 +6,38 @@
 # @FileName: 674. Longest Continuous Increasing Subsequence.py
 # @IDE: PyCharm
 ======================================="""
+# https://leetcode.com/problems/longest-continuous-increasing-subsequence/
+# Solution 1: O(n) Time and O(n) Space
+# Solution 2: O(n) Time and O(1) Space
+
+
+class Solution:
+    def findLengthOfLCIS1(self, nums):
+        n = len(nums)
+        if n == 0:
+            return 0
+        res = [1] * n
+        for i in range(n):
+            if i > 0 and nums[i] > nums[i - 1]:
+                res[i] = res[i - 1] + 1
+        return max(res)
+
+    def findLengthOfLCIS2(self, nums):
+        n = len(nums)
+        if n == 0:
+            return 0
+        res = 1
+        max_val = 1
+        for i in range(n):
+            if i > 0 and nums[i] > nums[i - 1]:
+                res += 1
+            else:
+                res = 1
+            max_val = max(max_val, res)
+        return max_val
+
+
+if __name__ == '__main__':
+    a = [1,3,5,4,7]
+    s = Solution()
+    print(s.findLengthOfLCIS1(a))           # Output is 3
