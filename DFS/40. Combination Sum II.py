de13c5bb13eaf5a6e9ed9905a8966effbb3716c8
fengyu586 <1349156262@qq.com>
2019-08-11 15:50:59 +0800

    # medium

---
+++ DFS/40. Combination Sum II.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""====================================
+# @Time    : 2019/8/11 15:20
+# @Author  : Jing
+# @FileName: 40. Combination Sum II.py
+# @IDE: PyCharm
+======================================="""
+# https://leetcode.com/problems/combination-sum-ii/
+
+
+class Solution:
+    def recursion(self, candidates, start_index, remain_target, combination, results):
+        if remain_target == 0:
+            if combination not in results:
+                results.append([i for i in combination])
+            return
+        for i in range(start_index, len(candidates)):
+            if remain_target < candidates[i]:
+                break
+            combination.append(candidates[i])
+            self.recursion(candidates, i + 1, remain_target - candidates[i], combination, results)
+            combination.pop()
+
+    def combinationSum2(self, candidates, target):
+        if not candidates:
+            return candidates
+        combination, results = [], []
+        candidates = sorted(candidates)
+        self.recursion(candidates, 0, target, combination, results)
+        return results
+
+
+if __name__ == '__main__':
+    a = [10,1,2,7,6,1,5]
+    target = 8
+    s = Solution()
+    print(s.combinationSum2(a, target))
+
