6e04b948c5f64f12b13c7748fdc9a55ecfa868f8
fengyu586 <1349156262@qq.com>
2019-08-16 15:13:33 +0800

    # hard

--- Dynamic Programming/354. Russian Doll Envelopes.py
+++ Dynamic Programming/354. Russian Doll Envelopes.py
@@ -8,6 +8,7 @@
 ======================================="""
 # https://leetcode.com/problems/russian-doll-envelopes/
 # Solution 1: O(n^2)Time O(n)Space
+# Solution 2: O(nlogn)Time O(n)Space
 
 
 class Solution:
@@ -24,8 +25,41 @@ class Solution:
                     dp[i] = max(dp[i], dp[j]+1)
         return max(dp)
 
+    def maxEnvelopes0(self, envelopes):
+        size = len(envelopes)
+        # 特判
+        if size < 2:
+            return size
+
+        # 对第一列排序，按照宽度排序
+        # 【特别注意】当宽度相等的时候，按照高度降序排序
+        # 以避免 [[11, 3], [12, 4], [12, 5], [12, 6], [14, 6]] 这种情况发生
+        # 正确排序 [[11, 3], [12, 6], [12, 5], [12, 4], [14, 6]]
+        envelopes.sort(key=lambda x: (x[0], -x[1]))
+
+        # print(envelopes)
+        tail = [envelopes[0][1]]
+
+        for i in range(1, size):
+            target = envelopes[i][1]
+            if target > tail[-1]:
+                tail.append(target)
+                continue
+
+            left = 0
+            right = len(tail) - 1
+
+            while left < right:
+                mid = (left + right) >> 1
+                if tail[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid
+            tail[left] = target
+        # print(tail)
+        return len(tail)
 
 if __name__ == '__main__':
-    a = [[5,4],[6,4],[6,7],[2,3]]
+    a = [[1,2],[2,3],[3,4],[3,5],[4,5],[5,5],[5,6],[6,7],[7,8]]
     s = Solution()
-    print(s.maxEnvelopes(a))                # Output is 3.
+    print(s.maxEnvelopes0(a))                # Output is 7.
