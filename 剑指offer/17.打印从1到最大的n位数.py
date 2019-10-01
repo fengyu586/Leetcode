f6531c7c4dcbba8cedb0975859c405a2c5be437a
fengyu586 <1349156262@qq.com>
2019-10-01 23:10:50 +0800

    # easy

--- 剑指offer/17.打印从1到最大的n位数.py
+++ 剑指offer/17.打印从1到最大的n位数.py
@@ -12,8 +12,8 @@ def str_add(string):
     take_over = 0
     for i in range(m-1, -1, -1):
         n_sum = int(string[i])
-        if i == n-1:
-            n_sum +=  1
+        if i == m-1:
+            n_sum += 1
         else:
             n_sum += take_over
         if n_sum >= 10:
@@ -65,11 +65,74 @@ def print_max_of_n_digit2(m):
     del string
 
 
+def add_str1(nums0, nums1, flag):
+    m, n = len(nums0), len(nums1)
+    nums1 = ['0' for _ in range(m-n)]+nums1
+    take_over = 0
+    if not flag:
+        # 相减
+        for i in range(m-1, -1, -1):
+            if i == -1:
+                diff = int(nums0[i])-int(nums1[i])
+            else:
+                diff = int(nums0[i])-int(nums1[i])-take_over
+            if diff < 0:
+                take_over = 1
+                nums0[i] = str(diff+10)
+            else:
+                nums0[i] = str(diff)
+                take_over = 0
+    else:
+        for i in range(m-1, -1, -1):
+            if i == -1:
+                diff = int(nums0[i])+int(nums1[i])
+            else:
+                diff = int(nums0[i])+int(nums1[i])+take_over
+            if diff > 10:
+                take_over = 1
+                nums0[i] = str(diff-10)
+            else:
+                nums0[i] = str(diff)
+                take_over = 0
+    for i in range(m):
+        if nums0[i] != '0':
+            return nums0[i:]
+    return '0'
+
+
+def str_add1(nums0, nums1):
+    nums0 = [s for s in str(nums0)]
+    nums1 = [s for s in str(nums1)]
+    flag0, flag1 = 1, 1
+    if nums0[0] == '-':
+        flag0 = -1
+        nums0 = nums0[1:]
+    if nums1[0] == '-':
+        flag1 = -1
+        nums1 = nums1[1:]
+    m, n = len(nums0), len(nums1)
+    if m < n:
+        nums0, nums1 = nums1, nums0
+        flag0, flag1 = flag1, flag0
+    elif m == n:
+        for i in range(n):
+            if nums0[i] < nums1[i]:
+                nums0, nums1 = nums1, nums0
+                flag0, flag1 = flag1, flag0
+                break
+    if flag0 == flag1:
+        flag = 1
+    else:
+        flag = 0
+    res = add_str1(nums0, nums1, flag)
+    if flag0 == -1:
+        res = ['-']+res
+    print(''.join(res))
+
+
 if __name__ == '__main__':
-    n = 2
-    print_max_of_n_digit2(n)
-
-
-
-
-
+    # n = 2
+    str0 = 0
+    str1 = -0
+    str_add1(str0, str1)
+    # print_max_of_n_digit2(n)
