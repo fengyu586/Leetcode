8211e0420e8c9d8d59bac6ae26981397743a74d7
fengyu586 <1349156262@qq.com>
2019-10-04 00:13:41 +0800

    # easy

--- 剑指offer/27.二叉树的镜像.py
+++ 剑指offer/27.二叉树的镜像.py
@@ -2,7 +2,8 @@
 # @Author  : Jing
 # @FileName: 27.二叉树的镜像.py
 # @IDE: PyCharm
-
+# Solution 1: Recursively
+# Solution 2: Iteratively
 
 class TreeNode:
     def __init__(self, x):
@@ -13,7 +14,7 @@ class TreeNode:
 
 class Solution:
     # 返回镜像树的根节点
-    def Mirror(self, root):
+    def Mirror1(self, root):
         # write code here
         if not root:
             return None
@@ -21,6 +22,22 @@ class Solution:
             return None
         root.left, root.right = root.right, root.left
         if root.left:
-            self.Mirror(root.left)
+            self.Mirror1(root.left)
         if root.right:
-            self.Mirror(root.right)
+            self.Mirror1(root.right)
+
+    def Mirror12(self, root):
+        # write code here
+        if not root:
+            return None
+        if not root.left and not root.right:
+            return root
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            node.left, node.right = node.right, node.left
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+        return root
