b2d68c4fcee63e36277b71059e743f8d65acdc05
fengyu586 <1349156262@qq.com>
2019-10-12 22:50:36 +0800

    # easy

--- 剑指offer/37.序列化二叉树.py
+++ 剑指offer/37.序列化二叉树.py
@@ -29,7 +29,8 @@ def reConstructBinaryTree(pre, tin):
 
 class Solution:
     def __init__(self):
-        self.flag = -1
+        self.pre_flag = -1
+        self.post_flag = None
 
     def pre_order(self, root):
         if not root:
@@ -41,23 +42,91 @@ class Solution:
         s += left + right
         return s
 
-    def Serialize(self, root):
+    def in_order(self, root):
+        if root is None:
+            s = '#,'
+            return s
+        s = self.in_order(root.left)
+        s += str(root.val)+','
+        s += self.in_order(root.right)
+        return s
+
+    def post_order(self, root):
+        if not root:
+            s = '#,'
+            return s
+        left = self.post_order(root.left)
+        right = self.post_order(root.right)
+        s = str(root.val)+','
+        s = left+right+s
+        return s
+
+    def pre_order_Serialize(self, root):
         # write code here
         s = self.pre_order(root)
         return s
 
-    def Deserialize(self, s):
-        # write code here
-        self.flag += 1
-        l = s.split(',')
-        if self.flag >= len(s):
+    def in_order_Serialize(self, root):
+        s = self.in_order(root)
+        return s
+
+    def post_order_Serialize(self, root):
+        s = self.post_order(root)
+        return s
+
+    def pre_deserialize(self, s):
+        self.pre_flag += 1
+        if self.pre_flag >= len(s):
             return None
         node = None
-        if l[self.flag] != '#':
-            node = TreeNode(int(l[self.flag]))
-            node.left = self.Deserialize(s)
-            node.right = self.Deserialize(s)
+        if s[self.pre_flag] != '#':
+            node = TreeNode(int(s[self.pre_flag]))
+            node.left = self.pre_deserialize(s)
+            node.right = self.pre_deserialize(s)
         return node
+
+    def pre_order_Deserialize(self, s):
+        # write code here
+        s = s.split(',')
+        return self.pre_deserialize(s)
+
+    def in_order_deserialize(self, s, start, end):
+        flag = (start+end) >> 1
+        if s[flag] == '#':
+            return None
+        left = self.in_order_deserialize(s, start, flag-1)
+        node = TreeNode(int(s[flag]))
+        right = self.in_order_deserialize(s, flag+1 ,end)
+        if left:
+            node.left = left
+        if right:
+            node.right = right
+        return node
+
+    def in_order_Deserialize(self, s):
+        # 中序遍历
+        s = s.split(',')
+        node = self.in_order_deserialize(s, 0, len(s)-1)
+        return node
+
+    def post_order_deserialize(self, s):
+        self.post_flag -= 1
+        if s[self.post_flag] == '#':
+            return None
+        root = TreeNode(int(s[self.post_flag]))
+        right = self.post_order_deserialize(s)
+        if right:
+            root.right = right
+        left = self.post_order_deserialize(s)
+        if left:
+            root.left = left
+        return root
+
+    def post_order_Deserialize(self, s):
+        s = s.split(',')
+        self.post_flag = len(s)-1
+        root = self.post_order_deserialize(s)
+        return root
 
 
 if __name__ == '__main__':
@@ -65,10 +134,10 @@ if __name__ == '__main__':
     inOrder = [1, 2, 3, 4, 5, 6, 7]
     flag = reConstructBinaryTree(preOrder, inOrder)
     s = Solution()
-    string = s.Serialize(flag)
+    string = '#,8,#,6,#,10,#,5,#,7,#,9,#,11,#'
     print(string)
-    a = s.Deserialize(string)
-    print(s.Serialize(a))
+    a = s.in_order_Deserialize(string)
+    print(s.in_order_Serialize(a))
 
 
 
