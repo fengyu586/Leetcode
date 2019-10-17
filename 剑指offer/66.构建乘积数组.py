793974dee4af27762205512f66d922ec4ab50dc7
fengyu586 <1349156262@qq.com>
2019-10-17 14:18:21 +0800

    # easy

--- 剑指offer/66.构建乘积数组.py
+++ 剑指offer/66.构建乘积数组.py
@@ -5,6 +5,7 @@
 # Solution 1: 暴力法 O(n^2)Time O(n)Space
 # Solution 2: 记忆化数组 O(n)Time O(n)Space
 
+
 class Solution:
     def sub_multiply(self, A, start, end):
         res = 1
@@ -19,7 +20,8 @@ class Solution:
         n = len(A)
         B = list(range(n))
         for i in range(n):
-            B[i] = self.sub_multiply(A, 0, i - 1) * self.sub_multiply(A, i + 1, n - 1)
+            B[i] = self.sub_multiply(A, 0, i - 1) * \
+                self.sub_multiply(A, i + 1, n - 1)
         return B
 
     def multiply2(self, A):
@@ -27,10 +29,10 @@ class Solution:
             return A
         n = len(A)
         C, D = list(range(n)), list(range(n))
-        C[0], D[n-1] = 1, 1
+        C[0], D[n - 1] = 1, 1
         for i in range(1, n):
-            C[i] = C[i-1]*A[i-1]
-            D[n-1-i] = D[n-i]*A[n-i]
+            C[i] = C[i - 1] * A[i - 1]
+            D[n - 1 - i] = D[n - i] * A[n - i]
         for i in range(n):
             C[i] *= D[i]
         return C
@@ -40,4 +42,3 @@ if __name__ == '__main__':
     nums = list(range(1, 11))
     s = Solution()
     print(s.multiply2(nums))
-
