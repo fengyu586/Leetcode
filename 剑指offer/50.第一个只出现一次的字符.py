a172f6858abd1a8ae181eaaa9b16b9a681467231
fengyu586 <1349156262@qq.com>
2019-11-03 11:49:31 +0800

    # medium

---
+++ 剑指offer/50.第一个只出现一次的字符.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# @Author  : Jing
+# @FileName: 50.第一个只出现一次的字符.py
+# @IDE: PyCharm
+# Solution 1: map O(n)Time O(n)Space
+
+
+class Solution:
+    def first_one(self, nums):
+        dic = {}
+        for num in nums:
+            if num not in dic:
+                dic[num] = 1
+            else:
+                dic[num] += 1
+        for num in dic.keys():
+            if dic[num] == 1:
+                return num
+        return -1
+
+
+if __name__ == '__main__':
+    nums = [0, 1, 1, 2, 2, 3]
+    s = Solution()
+    print(s.first_one(nums))
